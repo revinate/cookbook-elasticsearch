5bebd657dd712711420ff74d4b31a865d7bef377
Wayne Huang <wayne530@gmail.com>
2013-07-24 21:55:49 -0700

    Use standard /var/run/elasticsearch.pid as pidfile

--- attributes/default.rb
+++ attributes/default.rb
@@ -32,8 +32,8 @@ default.elasticsearch[:path][:conf] = "/usr/local/etc/elasticsearch"
 default.elasticsearch[:path][:data] = "/usr/local/var/data/elasticsearch"
 default.elasticsearch[:path][:logs] = "/usr/local/var/log/elasticsearch"
 
-default.elasticsearch[:pid_path]  = "/usr/local/var/run/elasticsearch"
-default.elasticsearch[:pid_file]  = "#{node.elasticsearch[:pid_path]}/#{node.elasticsearch[:node][:name].to_s.gsub(/\W/, '_')}.pid"
+default.elasticsearch[:pid_path]  = "/var/run"
+default.elasticsearch[:pid_file]  = "#{node.elasticsearch[:pid_path]}/elasticsearch.pid"
 
 # === MEMORY
 #
